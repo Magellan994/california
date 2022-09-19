44099b397fa4430adcb137855720bec5183e73b8
Magellan994 <khukha994@gmail.com>
2022-09-19 18:40:28 +0300

    changes 19_09_22 newq

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:3
+            },
+
+            1000:{
+                items:1
+            }
+        }
+    });
+});
+var trigger = document.getElementById('toggle');
+var box = document.getElementById('menu');
+
+toggle.addEventListener('click', function() {
+    box.classList.toggle('active');
+});
